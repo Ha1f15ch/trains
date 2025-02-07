486d406870eed58f99f7608a79c80f2b766790de
alexy.alexy98@mail.ru <alexy.alexy98@mail.ru>
2025-02-08 01:20:32 +0300

    Дописал реализацию для метода - GetAllPostsByPartTitle. Завершил реализацию для класса - NewsChannelsPostsRepository.

--- ApplicationDatabaseContext/DatabaseEngine/RepositoryStorage/Repositories/NewsChannelsPostsRepository.cs
+++ ApplicationDatabaseContext/DatabaseEngine/RepositoryStorage/Repositories/NewsChannelsPostsRepository.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DatabaseEngine.RepositoryStorage.Repositories
 {
@@ -23,37 +24,45 @@ namespace DatabaseEngine.RepositoryStorage.Repositories
 
         public async Task<NewsChannelsPosts?> CreateNewNewsChannelsPost(int newChannelId, string titlePost, string bodyPost, string? footerPost, string authorPost, string? surceImage)
         {
-            var newsChannel = await _appDbContext.NewsChannels.FindAsync(newChannelId);
-
-            if(newsChannel is null)
+            try
             {
-                _logger.LogWarning($"Некорректное значение id новостного канала - {nameof(newsChannel)}");
-                return null;
-            }
+				var newsChannel = await _appDbContext.NewsChannels.FindAsync(newChannelId);
 
-            if(string.IsNullOrEmpty(titlePost) || string.IsNullOrEmpty(bodyPost) || string.IsNullOrEmpty(authorPost))
+				if (newsChannel is null)
+				{
+					_logger.LogWarning($"Некорректное значение id новостного канала - {nameof(newsChannel)}");
+					return null;
+				}
+
+				if (string.IsNullOrEmpty(titlePost) || string.IsNullOrEmpty(bodyPost) || string.IsNullOrEmpty(authorPost))
+				{
+					_logger.LogWarning($"Некорректное значение для параметра titlePost = {titlePost} - {nameof(titlePost)}\nили bodyPost = {bodyPost} - {nameof(bodyPost)}\nили authorPost = {authorPost} - {nameof(authorPost)}\nили");
+					return null;
+				}
+
+				var newPost = new NewsChannelsPosts
+				{
+					NewsChannelId = newsChannel.Id,
+					TitlePost = titlePost,
+					BodyPost = bodyPost,
+					FooterPost = footerPost,
+					AuthorPost = authorPost,
+					SurceImage = surceImage,
+					CreatedDate = DateTime.UtcNow
+				};
+
+				await _appDbContext.NewsChannelsPosts.AddAsync(newPost);
+				await _appDbContext.SaveChangesAsync();
+
+				_logger.LogInformation($"Запись новостного сообщения была создана. newPost = {nameof(newPost)}. Где newPost.NewsChannelId = {newPost.NewsChannelId}\nnewPost.TitlePost = {newPost.TitlePost}\nnewPost.BodyPost = {newPost.BodyPost}\nnewPost.FooterPost = {newPost.FooterPost}\nnewPost.AuthorPost = {newPost.AuthorPost}\nnewPost.SurceImage = {newPost.SurceImage}\nnewPost.CreatedDate = {newPost.CreatedDate}");
+
+				return newPost;
+			}
+            catch( Exception ex )
             {
-                _logger.LogWarning($"Некорректное значение для параметра titlePost = {titlePost} - {nameof(titlePost)}\nили bodyPost = {bodyPost} - {nameof(bodyPost)}\nили authorPost = {authorPost} - {nameof(authorPost)}\nили");
-                return null;
-            }
-
-            var newPost = new NewsChannelsPosts 
-            {
-				NewsChannelId = newsChannel.Id,
-                TitlePost = titlePost,
-                BodyPost = bodyPost,
-                FooterPost = footerPost,
-                AuthorPost = authorPost,
-                SurceImage = surceImage,
-                CreatedDate = DateTime.UtcNow
-            };
-
-            await _appDbContext.NewsChannelsPosts.AddAsync(newPost);
-            await _appDbContext.SaveChangesAsync();
-
-            _logger.LogInformation($"Запись новостного сообщения была создана. newPost = {nameof(newPost)}. Где newPost.NewsChannelId = {newPost.NewsChannelId}\nnewPost.TitlePost = {newPost.TitlePost}\nnewPost.BodyPost = {newPost.BodyPost}\nnewPost.FooterPost = {newPost.FooterPost}\nnewPost.AuthorPost = {newPost.AuthorPost}\nnewPost.SurceImage = {newPost.SurceImage}\nnewPost.CreatedDate = {newPost.CreatedDate}");
-
-		    return newPost;
+				_logger.LogError(ex, $"При выполнении метода {nameof(CreateNewNewsChannelsPost)} возникла ошибка: {ex.Message}");
+				throw;
+			}
         }
 
         public async Task<List<NewsChannelsPosts>?> GetAllPosts()
@@ -79,24 +88,54 @@ namespace DatabaseEngine.RepositoryStorage.Repositories
 
                 if(newsChannel is null)
                 {
-                    _logger.LogWarning($"Передано некорректное значение id новостного канала - {newsChannelId} - {nameof(newsChannelId)}. Не найден новостной канал п означению - {newsChannelId}");
+                    _logger.LogWarning($"Передано некорректное значение id новостного канала - {newsChannelId} - {nameof(newsChannelId)}. Не найден новостной канал по значению id - {newsChannelId}");
                     return null;
                 }
 
                 _logger.LogInformation($"Поиск постов для новостного канала - {newsChannel} - {nameof(newsChannel)}");
-                return await _appDbContext.NewsChannelsPosts.Where(el => el.NewsChannelId == newsChannel.Id).ToListAsync();
+                var listPosts = await _appDbContext.NewsChannelsPosts.Where(el => el.NewsChannelId == newsChannel.Id).ToListAsync();
 
+                if(!listPosts.Any())
+                {
+					_logger.LogWarning($"Постов от новостного канала с id = {newsChannelId} не найдено.");
+					return null;
+                }
+
+                return listPosts;
             }
             catch(Exception ex)
             {
-                _logger.LogWarning($"Возникла ошибка при поиске постов для новостного канала id = {newsChannelId} - {ex.Message}");
+                _logger.LogError($"Возникла ошибка при поиске постов для новостного канала id = {newsChannelId} - {ex.Message}");
                 throw;
             }
         }
 
         public async Task<List<NewsChannelsPosts>?> GetAllPostsByPartTitle(string titlePost)
         {
-            throw new NotImplementedException();
+			try
+            {
+				if (string.IsNullOrEmpty(titlePost))
+				{
+					_logger.LogWarning($"Некорректное значение titlePost поста новостного канала: {titlePost}. Ожидается не null.");
+					return null;
+				}
+
+				_logger.LogInformation($"Поиск постов по заголовку = {titlePost}, вызван метод {nameof(GetAllPostsByPartTitle)}");
+                var listPostByPartTitlePost = await _appDbContext.NewsChannelsPosts.Where(el => el.TitlePost.Contains(titlePost)).ToListAsync();
+
+                if(!listPostByPartTitlePost.Any())
+                {
+                    _logger.LogWarning($"Не найдено совпадений с заданной частью заголовка - {titlePost}");
+                }
+
+                return listPostByPartTitlePost;
+			}
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"При выполнении метода {nameof(GetAllPostsByPartTitle)} возникла ошибка: {ex.Message}");
+
+				throw;
+            }
         }
     }
 }
