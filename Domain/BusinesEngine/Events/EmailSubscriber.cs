f3121f48f56dffb9e711fe686cf14a7792cfee37
alexy.alexy98@mail.ru <alexy.alexy98@mail.ru>
2025-02-11 02:38:36 +0300

    Правки для настройки отправки уведомлений пользователям на почту

---
+++ Domain/BusinesEngine/Events/EmailSubscriber.cs
@@ -0,0 +1,31 @@
+﻿using BusinesEngine.Events.Interfaces;
+using BusinesEngine.Services.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinesEngine.Events
+{
+	public class EmailSubscriber : INewsSubscriber
+	{
+		private readonly IEmailService _emailService;
+
+		public EmailSubscriber(IEmailService emailService)
+		{
+			_emailService = emailService;
+		}
+
+		public async Task Update(string message)
+		{
+			await Task.Run(async () =>
+			{
+				var subject = "Новое сообщение !";
+				var body = message;
+
+				await _emailService.SendEmailAsync("alexy.alexy98@mail.ru", subject, body);
+			});
+		}
+	}
+}
---
+++ Domain/BusinesEngine/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using BusinesEngine.Services.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinesEngine.Services
+{
+	public class EmailService : IEmailService
+	{
+		public async Task SendEmailAsync(string email, string subject, string message)
+		{
+			using (var smtpClient = new SmtpClient("smtp.example.com"))
+			{
+				var mailMessage = new MailMessage("from@example.com", email, subject, message);
+				await smtpClient.SendMailAsync(mailMessage);
+			}
+		}
+	}
+}
