137c4d6c1a0770e7d620340a1bc6a656d8998c1a
alexy.alexy98@mail.ru <alexy.alexy98@mail.ru>
2025-03-15 10:38:12 +0300

    #4 Поправил логирование и валидацию параметров в классе SubscriptionRepository

--- ApplicationDatabaseContext/DatabaseEngine/RepositoryStorage/Repositories/SubscriptionRepository.cs
+++ ApplicationDatabaseContext/DatabaseEngine/RepositoryStorage/Repositories/SubscriptionRepository.cs
@@ -22,17 +22,24 @@ namespace DatabaseEngine.RepositoryStorage.Repositories
             _mediator = mediator;
         }
 
+        //Поиск подписок пользователя по userId
         public async Task<List<Subscription>> GetUserSubscriptions(int userId)
         {
             try
             {
+                //Проверяем, есть ли пользователь
                 var userExists = await _appDbContext.Users.AnyAsync(u => u.Id == userId);
 
                 if(userExists)
                 {
                     _logService.LogInformation($"{nameof(GetUserSubscriptions)} - Выполняется поиск подписок пользователя - {userId}.");
-                    return await _appDbContext.Subscriptions.Where(sub => sub.UserId == userId).ToListAsync();
-                }
+                    
+                    var serSubscriptions = await _appDbContext.Subscriptions.Where(sub => sub.UserId == userId).ToListAsync();
+
+                    _logService.LogInformation($"Найдено записей - {serSubscriptions.Count}");
+
+                    return serSubscriptions;
+				}
 
                 throw new ArgumentException($"Пользователь с ID {userId} не найден.");
             }
@@ -47,43 +54,77 @@ namespace DatabaseEngine.RepositoryStorage.Repositories
         {
             try
             {
-                var subscriber = await _appDbContext.Users.FindAsync(userId);
-                var book = await _appDbContext.Books.FindAsync(bookId);
+				_logService.LogWarning($"Проверка входных параметров");
 
-                if(subscriber is not null && book is not null)
+				// Валидация параметра userId
+				if (userId <= 0)
                 {
-                    var existingSubscription = subscriber.Subscriptions.SingleOrDefault(subs => subs.BookId == book.Id);
-
-                    if(existingSubscription is null)
-                    {
-                        var newSubscription = new Subscription
-                        {
-                            UserId = userId,
-                            BookId = bookId,
-                            SubscriptionDate = DateTime.UtcNow,
-                        };
-
-                        await _appDbContext.AddAsync(newSubscription);
-                        await _appDbContext.SaveChangesAsync();
-
-                        var subscriptionDto = new SubscriptionDto
-                        {
-                            UserId = newSubscription.UserId,
-                            BookId = newSubscription.BookId,
-                            SubscriptionDate = newSubscription.SubscriptionDate
-                        };
-
-                        await _mediator.Publish(new SubscriptionCreatedEvent(subscriptionDto));
-
-                        _logService.LogInformation($"{nameof(SubscribeUserToBook)} - Выполняется оповещение пользователя {userId} о том, что подписка на книгу {bookId} выполнена успешно. Была создана запись подписки - {subscriptionDto} - {nameof(subscriptionDto)}");
-
-                        return newSubscription;
-                    }
-
-                    return existingSubscription;
+                    _logService.LogWarning($"Передано некорректное значение для userId.");
+                    throw new ArgumentException("Параметр userId не может быть меньше или равно 0");
                 }
 
-                throw new NullReferenceException($"При выполнении поиска пользователя и книги возникла ошибка. Получен null. User = {subscriber} - {nameof(subscriber)}. Book = {book} - {nameof(book)}");
+				// Валидация параметра bookId
+				if (bookId <= 0)
+				{
+					_logService.LogWarning($"Передано некорректное значение для bookId.");
+					throw new ArgumentException("Параметр bookId не может быть меньше или равно 0");
+				}
+
+                //Проверка, есть ли в контексте для данных id записи
+				var subscriber = await _appDbContext.Users.FindAsync(userId);
+                var book = await _appDbContext.Books.FindAsync(bookId);
+
+                if (subscriber is null)
+                {
+                    _logService.LogWarning($"Запись - subscriber не найдена. {subscriber}");
+                    throw new NullReferenceException($"Запись по userId не найдена. subscriber = null");
+                }
+
+                if(book is null)
+                {
+                    _logService.LogWarning($"Запись - book не найдена. {book}");
+					throw new NullReferenceException($"Запись по bookId не найдена. book = null");
+				}
+
+				_logService.LogInformation($"проверка, является ли пользователь с userId = {userId} подписчиком на книгу - {bookId}");
+
+				var existingSubscription = subscriber.Subscriptions.SingleOrDefault(subs => subs.BookId == book.Id);
+
+                //Если его нет
+                if(existingSubscription is null)
+                {
+                    _logService.LogInformation($"Создание новой записи подписки пользователя - {userId} на книгу - {bookId}");
+
+                    //Создание экземпляра подписки для сохранения в контексте
+                    var newSubscription = new Subscription
+                    {
+                        UserId = userId,
+                        BookId = bookId,
+                        SubscriptionDate = DateTime.UtcNow,
+                    };
+
+                    await _appDbContext.AddAsync(newSubscription);
+                    await _appDbContext.SaveChangesAsync();
+
+                    //Создание специального типа данных для передачи в класс обработчик, ожидающие этот тип данных
+                    var subscriptionDto = new SubscriptionDto
+                    {
+                        UserId = newSubscription.UserId,
+                        BookId = newSubscription.BookId,
+                        SubscriptionDate = newSubscription.SubscriptionDate
+                    };
+
+                    //Вызов события в сервисе mediatr, передача в него специального типа данных 
+                    await _mediator.Publish(new SubscriptionCreatedEvent(subscriptionDto));
+
+                    _logService.LogInformation($"{nameof(SubscribeUserToBook)} - Выполняется оповещение пользователя {userId} о том, что подписка на книгу {bookId} выполнена успешно. Была создана запись подписки {subscriptionDto} - {nameof(subscriptionDto)}");
+
+                    return newSubscription;
+                }
+
+				_logService.LogInformation($"Пользователь с userId = {userId} уже подписан на книгу boolId = {bookId}");
+
+				return existingSubscription;
             }
             catch(Exception ex)
             {
